dde67e388a50f07f25b1ff44151733125cfeecee
Sarthak <s.sarthak1357@gmail.com>
2025-01-22 00:31:35 +0530

    22-01-25

--- Weather App/script.js
+++ Weather App/script.js
@@ -10,18 +10,17 @@ const location_not_found = document.querySelector('.location-not-found');
 
 const weather_body = document.querySelector('.weather-body');
 
-
 async function checkWeather(city){
-    const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
+    const api_key = "aab1e8d86e533d896f902037167ce8d5";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
-
+    
+    console.log(weather_data)
 
     if(weather_data.cod === `404`){
         location_not_found.style.display = "flex";
         weather_body.style.display = "none";
-        console.log("error");
         return;
     }
 
@@ -34,13 +33,12 @@ async function checkWeather(city){
     humidity.innerHTML = `${weather_data.main.humidity}%`;
     wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
 
-
     switch(weather_data.weather[0].main){
         case 'Clouds':
             weather_img.src = "/assets/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "/assets/clear.png";
+            weather_img.src = "/assets/clear.jpg";
             break;
         case 'Rain':
             weather_img.src = "/assets/rain.png";
@@ -53,10 +51,7 @@ async function checkWeather(city){
             break;
 
     }
-
-    console.log(weather_data);
 }
-
 
 searchBtn.addEventListener('click', ()=>{
     checkWeather(inputBox.value);
