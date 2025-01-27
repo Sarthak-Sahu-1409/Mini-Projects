d93576e3424046319796c0707bcb83921d8c0fe4
Sarthak <s.sarthak1357@gmail.com>
2025-01-28 02:14:54 +0530

    28-01-25

---
+++ Movie Search App/Movie Search App/src/App.jsx
@@ -0,0 +1,26 @@
+import './css/App.css'
+import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
+import {Routes, Route} from 'react-router-dom'
+import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
+import './css/App.css'
+import { MovieProvider } from './contexts/MovieContext'
+
+function App() {
+  return (
+    <MovieProvider>
+    <div>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+        </Routes>
+      </main>
+      </div>
+    </MovieProvider>
+  )
+}
+
+export default App
---
+++ Movie Search App/Movie Search App/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "75568e6e803f0ee3f6ea43fe51c53713";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
